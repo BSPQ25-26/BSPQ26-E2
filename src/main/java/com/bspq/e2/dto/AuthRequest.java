b52758870981838f3d5471b743ad063ff310990c
Alexxis21 <aleksandraswiderek1@gmail.com>
2026-04-30 15:52:36 +0200

    validation changes

--- src/main/java/com/bspq/e2/dto/AuthRequest.java
+++ src/main/java/com/bspq/e2/dto/AuthRequest.java
@@ -1,7 +1,10 @@
 package com.bspq.e2.dto;
+import jakarta.validation.constraints.NotBlank;
 
 public class AuthRequest {
+    @NotBlank(message = "Username is required")
     private String username;
+    @NotBlank(message = "Password is required")
     private String password;
 
     public String getUsername() { return username; }
